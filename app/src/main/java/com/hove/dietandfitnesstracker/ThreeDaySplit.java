800f7a3fc16db8a032f25bb70ea688f6e0899dad
WisdomHove <hovewisdom@gmail.com>
2022-05-03 09:43:46 +0200

    Adding instructions for the exercises.

--- app/src/main/java/com/hove/dietandfitnesstracker/ThreeDaySplit.java
+++ app/src/main/java/com/hove/dietandfitnesstracker/ThreeDaySplit.java
@@ -2,7 +2,9 @@ package com.hove.dietandfitnesstracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ThreeDaySplit extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class ThreeDaySplit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three_day_split);
+    }
+
+    public void StartVideo(View view) {
+
+        Intent intent = new Intent(ThreeDaySplit.this, WebViewActivity.class);
+        startActivity(intent);
     }
 }
